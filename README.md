7f98c654b7fc40620bc3e447bbc89d82ba5c1c5b
Manish <man904u@gmail.com>
2025-04-05 14:44:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+The following Repo contains Code to Analyzes the padding consistency in a sequence of numeric strings.
+
+Library Used: Typescript
+
+## To setup
+
+* git clone https://github.com/cliffymk/quickcode
+* cd quickcode
+* npm i
+
+## To test
+
+* Run the below command
+> npm start
+
+* Enter the input array to test
---
+++ checkNumberPadding.ts
@@ -0,0 +1,34 @@
+// Checks if all number strings in a sequence have consistent padding.
+// Returns padding length if consistent, -1 if inconsistent, and -minLength if no padding at all.
+export function checkNumberPadding(intStrs: Iterable<string>): number {
+    if (!intStrs) {
+        throw new Error("Input should not be null or undefined.");
+    }
+
+    const paddedLengths = new Set<number>();
+    let hasPadding = false;
+    let minLength = Infinity;
+
+    for (const str of intStrs) {
+        if (typeof str !== 'string') {
+            throw new Error(`Expected string, got ${typeof str}`);
+        }
+
+        if (!/^\d+$/.test(str)) {
+            throw new Error(`Invalid numeric string: "${str}"`);
+        }
+
+        if (/^0+/.test(str)) {
+            hasPadding = true;
+            paddedLengths.add(str.length);
+        }
+
+        minLength = Math.min(minLength, str.length);
+    }
+
+    if (hasPadding) {
+        return paddedLengths.size === 1 ? [...paddedLengths][0] : -1;
+    }
+
+    return -minLength;
+}
